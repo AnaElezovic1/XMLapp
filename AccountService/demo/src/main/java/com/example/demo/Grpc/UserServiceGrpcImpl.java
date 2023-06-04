c3494ef372262e05158ca0aaf5b53fde7b26aaed
Frozennn <39025374+Frozennn@users.noreply.github.com>
2023-06-04 16:28:39 +0200

    fix main klasa dodat delete za grpc

--- AccountService/demo/src/main/java/com/example/demo/Grpc/UserServiceGrpcImpl.java
+++ AccountService/demo/src/main/java/com/example/demo/Grpc/UserServiceGrpcImpl.java
@@ -4,6 +4,7 @@ import com.example.demo.service.UserService;
 import com.example.demo.user.*;
 import com.example.demo.user.UserAppServiceGrpc;
 import io.grpc.stub.StreamObserver;
+import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -38,5 +39,34 @@ public class UserServiceGrpcImpl extends UserAppServiceGrpc.UserAppServiceImplBa
 
         responseObserver.onNext(response);
         responseObserver.onCompleted();
+    }
+
+    @Override
+    public void addUser(AddUserRequest request, StreamObserver<AddUserResponse> responseObserver){
+
+
+
+      UserApp newUser = mapUser(request.getUser());
+      userService.addNewUser(newUser);
+
+      AddUserResponse response = AddUserResponse.newBuilder().setSuccess(true).build();
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+       
+
+    }
+
+    private UserApp mapUser(com.example.demo.user.UserApp user){
+        UserApp mappedUser = new UserApp();
+        mappedUser.setId(user.getId());
+        mappedUser.setUsername(user.getUsername());
+        mappedUser.setPassword(user.getPassword());
+        mappedUser.setEmail(user.getEmail());
+        mappedUser.setRole(com.example.demo.model.Role.valueOf(user.getRole().name()));
+        mappedUser.setAdress(user.getAddress());
+        System.out.println("Mapped User Username: " + mappedUser.getUsername());
+        System.out.println("Mapped User: " + mappedUser.toString());
+        return mappedUser;
     }
 }
