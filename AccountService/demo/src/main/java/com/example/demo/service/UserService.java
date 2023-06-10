fe8a2202681858d8b1571eab5180a2399d9604a6
Frozennn <39025374+Frozennn@users.noreply.github.com>
2023-06-10 17:17:53 +0200

    aba

--- AccountService/demo/src/main/java/com/example/demo/service/UserService.java
+++ AccountService/demo/src/main/java/com/example/demo/service/UserService.java
@@ -2,8 +2,13 @@ package com.example.demo.service;
 
 import com.example.demo.model.UserApp;
 import com.example.demo.repository.UserAppRepository;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.StatusRuntimeException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.example.demo.accomodation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,12 +16,34 @@ import java.util.Optional;
 @Service
 public class UserService {
 
+    private final AccomodationServiceGrpc.AccomodationServiceBlockingStub accomodationServiceStub;
 
     private final UserAppRepository userRepository;
 
     @Autowired
     public UserService(UserAppRepository userRepository) {
         this.userRepository = userRepository;
+        ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 4111).usePlaintext().build();
+        this.accomodationServiceStub = AccomodationServiceGrpc.newBlockingStub(channel);
+    }
+
+    public void createAccommodation(Accomodation accommodation) {
+        // Pozivanje gRPC metoda Create iz AccomodationService
+        Accomodation request = Accomodation.newBuilder()
+                .setId(accommodation.getId())
+                .setName(accommodation.getName())
+                .setDescription(accommodation.getDescription())
+                .setLocation(accommodation.getLocation())
+                .setImages(accommodation.getImages())
+                .setBeds(accommodation.getBeds())
+                .build();
+
+        try {
+            accomodationServiceStub.create(request);
+            System.out.println("Smeštaj je uspešno kreiran.");
+        } catch (StatusRuntimeException e) {
+            System.out.println("Greška prilikom kreiranja smeštaja: " + e.getStatus());
+        }
     }
 
     public void addNewUser(UserApp users){
