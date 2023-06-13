77585c7568a172251ceddb76aff8fa6d3de0c289
AnaElezovic1 <101984815+AnaElezovic1@users.noreply.github.com>
2023-06-13 10:48:14 +0200

    Odradjena je autentifikacija,logovanje, i registrovanje korisnika

---
+++ AccountService/demo/src/main/java/com/example/demo/model/Host.java
@@ -0,0 +1,19 @@
+package com.example.demo.model;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Host extends UserApp {
+
+    public Host(UserApp userApp) {
+        super(userApp.getId(), userApp.getUsername(), userApp.getEmail(), userApp.getPassword(), userApp.getPasswordSalt(), userApp.getAddress(), userApp.getActive(), userApp.getRole());
+    }
+}
--- AccountService/demo/src/main/java/com/example/demo/model/Role.java
+++ AccountService/demo/src/main/java/com/example/demo/model/Role.java
@@ -1,22 +1,34 @@
 package com.example.demo.model;
 
-import jakarta.persistence.*;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonIncludeProperties;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
 
+import javax.persistence.*;
 
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIncludeProperties({"name"})
+public class Role implements GrantedAuthority {
 
-public enum Role {
-    NEREGISTROVANI_KORISNIK("NK"),
-    GOST("G"),
-    HOST("H");
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    @Column
+    private String name;
 
-    private final String user;
-
-    Role(String user) {
-        this.user = user;
+    @Override
+    public String getAuthority() {
+        return name;
     }
 }
