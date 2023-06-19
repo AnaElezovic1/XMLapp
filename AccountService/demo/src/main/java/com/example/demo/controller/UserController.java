e336d699a73ef0488a8678148a06382baa238d68
AnaElezovic1 <101984815+AnaElezovic1@users.noreply.github.com>
2023-06-19 20:11:36 +0200

    kontroler

--- AccountService/demo/src/main/java/com/example/demo/controller/UserController.java
+++ AccountService/demo/src/main/java/com/example/demo/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import com.example.demo.dto.RateDTO;
+import com.example.demo.dto.RateForAccDto;
 import com.example.demo.model.UserApp;
 import com.example.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -49,6 +50,55 @@ public class UserController {
     public void rateHost(@PathVariable Long hostId, @RequestBody RateDTO rateDTO){
         userService.rateHost(hostId,rateDTO);
     }
+
+
+    @PostMapping("/{id}/rateAcc")
+    public void rateAcc(@PathVariable Long id, @RequestBody RateForAccDto rateDTO){
+        userService.rateAccommodation(id,rateDTO);
+    }
+
+    @PutMapping("/{hostId}/update")
+    public void updateRate(@PathVariable Long hostId, @RequestBody RateDTO rateDTO){
+        userService.updateRate(hostId, rateDTO);
+    }
+
+    @PutMapping("/{rateId}/updateAcc")
+    public void updateRate(@PathVariable Long rateId, @RequestBody RateForAccDto rateDTO){
+        userService.updateRateAcc(rateId, rateDTO);
+    }
+
+    @DeleteMapping("/delete/{rateId}")
+    public void deleteRate(@PathVariable Long rateId){
+        userService.deleteRate(rateId);
+    }
+
+    @DeleteMapping("/deleteAcc/{rateId}")
+    public void deleteAcc(@PathVariable Long rateId){
+        userService.deleteRateForAcc(rateId);
+    }
+
+    @GetMapping("/{rateId}/getRate")
+    public void getRate(@PathVariable Long rateId){
+        userService.getByIdRate(rateId);
+    }
+
+    @GetMapping("/{rateId}/getRateAcc")
+    public void getRateAcc(@PathVariable Long rateId){
+        userService.getByIdRateAcc(rateId);
+    }
+
+    @GetMapping("/avgRate")
+    public void getAvgRate(){
+        userService.averageRate();
+    }
+    @GetMapping("/avgRateAcc")
+    public void getAvgRateAcc(){
+        userService.averageRateAcc();
+    }
+
+
+
+
 
 
 
